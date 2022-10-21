40c45a3a60726700a87c2cf7f94947ab2ad39d0f
cwilson93 <cwilson2@chicagobooth.edu>
2022-10-21 21:24:02 +0000

    one more

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,27 @@ class ApplicationController < ActionController::Base
     render({:template => "calculation_templates/square_form.html.erb"})
   end
 
+  def blank_square_root_form
+
+    render({:template => "calculation_templates/square_root_form.html.erb"})
+  end
+
+  def rand_form
+
+    render({:template => "calculation_templates/rand_form.html.erb"})
+  end
+
+
   def calculate_square
     @num = params.fetch("elephant").to_f
     @square_of_num = @num ** 2
     render({:template => "calculation_templates/square_results.html.erb"})
+  end
+
+  def calculate_square_root
+    @num = params.fetch("elephant").to_f
+    @square_root_of_num = @num ** (1/2.0)
+    render({:template => "calculation_templates/square_root_results.html.erb"})
   end
 
   def calculate_random
